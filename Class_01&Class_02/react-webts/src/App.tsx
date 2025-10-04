254e0d3e04fa7fcb42bc3d23bea068ede6e42687
AribaMemon <codewitharibamemon@gmail.com>
2025-10-04 16:20:25 +0500

    privatewebcourseclass01and02

---
+++ Class_01&Class_02/react-webts/src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+  return (
+    <>
+   <div>
+    <h1 className="bg-primary text-white font-interbold" >Hello anum</h1>
+    <p className="font-intersemibold">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur iure veritatis repellat omnis sapiente quod? Cumque voluptates iste pariatur nostrum commodi, consequatur officia, nam earum aut dolorem magnam delectus tenetur!</p>
+   </div>
+    </>
+  )
+}
+
+export default App
---
+++ Class_01&Class_02/react-webts/src/pages/root.tsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+
+
+const root = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <Footer />
+
+    </div>
+  )
+}
+
+export default root
