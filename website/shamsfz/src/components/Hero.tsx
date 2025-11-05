46a2cd7b623c8c6bc5e572eeddf2ad4f213afff2
AribaMemon <codewitharibamemon@gmail.com>
2025-11-05 13:31:05 +0500

    code

--- website/shamsfz/src/components/Hero.tsx
+++ website/shamsfz/src/components/Hero.tsx
@@ -2,11 +2,29 @@
 //public key = " eE77jpwJwymSL6YDy"
 // service id ="service_z20u8pv"
 //number variable
+import Googlereview from "../assets/images/Googlereview.jpg"
+import businesscenter from "../assets/images/shams-hero-image.webp"
 const Hero = () => {
   return (
-    <div>
-      Hero
+   <section>
+    <div className="items-center m-10">
+      <img src={Googlereview} width={300} height={350}  alt="Googlereview"/>
+      <div>
+        <h1>Start Strong, <br />Build Without Limits</h1>
+        <h3>A Free Zone Built for the Future of Business</h3>
+        <p>Shams Free Zone gives modern entrepreneurs everything they need to <br />
+        launch and grow, from fast, digital business setup to flexible licensing
+        <br />that adapts to your goals.</p>
+      </div>
+      <form >
+        <input type="number"  placeholder="+971 xx xxx xxxx"/>
+      </form>
+      <p>Receive a callback in less than 60 seconds</p>
     </div>
+    <div>
+      <img src={businesscenter} width={600} height={750} alt="Businesscenter"/>
+    </div>
+   </section>
   )
 }
 
--- website/shamsfz/src/components/Navbar.tsx
+++ website/shamsfz/src/components/Navbar.tsx
@@ -8,8 +8,8 @@ import {X,Search,User} from "lucide-react"
 const Navbar = () => {
   return (
    <>
-   <header>
-    <div  className=''>
+   <header className='w-full bg-white shadow-lg'>
+    <div  className='max-w-6xl mx-auto px-1 py-1 flex items-center justify-between'>
       {/* *************** Logo /nav Div  Desktop  ********************** */}
    <div className='flex  items-center m-4  '>
        <img src={logo} width={120}  height={100} alt='logo'/>
@@ -25,28 +25,37 @@ const Navbar = () => {
         <Link  className='font-interbold hover:font-interregular hover:color-primary hover:cursor-pointer'to="/services">Services</Link>
   
     </nav>
+    
     </div>
-  <div className='flex gap-4 items-center'>
-    <button className='rounded-full   hover:'> <Search  className='' size={18}/> </button>
+ 
+ <div className='flex gap-8 items-center '>
+    <button className='rounded-full bg-gray-400 p-2  m-4 hover:bg-secondary'> <Search  className='hover:text-white' size={18}/> </button>
 
     {/* <BiSolidSearch /> */}
     {/* الإنجليزية */}
-  <Link className='font-interbold hover:font-interregular hover:color-primary hover:cursor-pointer'  to="/services">AR</Link>
-  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+    {/* */}
+<span > <Link className='font-interbold hover:font-interregular hover:color-primary hover:cursor-pointer'  to="/services">AR</Link>   </span>
+ 
+<div className='items-center gap-1 flex cursor-pointer'>
+   <User  size={18}/>
  
 {/* تسجيل الدخول */}
  <SignedOut>
-        <SignInButton />
+  
+        <SignInButton  mode='modal'/>
+      
       </SignedOut>
-      <SignedIn>
+      <SignedIn >
+      
         <UserButton />
+
       </SignedIn>
+</div>
       {/* ابدأ */}
-  <button>
-    <Link to={"/getstarted"}>Get Started</Link>
+  <button className='border transition delay-150 duration-300 ease-in-out hover:-translate-y-1 border-primary text-secondary px-5 py-2 rounded-full hover:bg-secondary hover:text-white '>
+    <Link to={"/contact-us"}>Get Started</Link>
   </button>
   </div>
-
   </div>
 <div>
        </div>
